151dbf0cbc0edb69e8f684fc2ae13e416c70e2a5
Tejas Tomar <tejastomar2002@gmail.com>
2022-02-11 00:21:08 +0530

    second commit

---
+++ index.js
@@ -0,0 +1,56 @@
+//selectors
+
+const todoInput=document.querySelector('.todo-input');
+const todoButton=document.querySelector('.todo-button');
+const todoList=document.querySelector('.todo-list');
+
+//event listeners
+
+todoButton.addEventListener('click',addTodo);
+todoList.addEventListener('click',deleteCheck);
+
+//functions
+function addTodo(event){
+    //prevent form from submitting 
+    event.preventDefault();
+    const todoDiv=document.createElement("div");
+    todoDiv.classList.add("todo");
+    //Create LI
+    const newTodo =document.createElement('li');
+    newTodo.innerText=todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    //checkmark button
+    const completedButton=document.createElement('button');
+    completedButton.innerHTML='<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+    //check trash button
+    const trashButton=document.createElement('button');
+    trashButton.innerHTML='<i class="fas fa-trash"></i>';
+    trashButton.classList.add('trash-btn');
+    todoDiv.appendChild(trashButton);
+    //append to list
+    todoList.appendChild(todoDiv);
+    //clear
+    todoInput.value="";
+}
+
+function deleteCheck(e)
+{
+   const item=e.target;
+   //delete todo
+   if(item.classList[0]==='trash-btn')
+   {
+       const todo=item.parentElement;
+       todo.remove();
+   }
+
+   //checkmark
+   if(item.classList[0]==='complete-btn'){
+       const todo=item.parentElement;
+       todo.classList.toggle('completed');
+   }
+   
+}
+
